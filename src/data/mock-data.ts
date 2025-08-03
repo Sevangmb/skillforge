923e23cac0aaaaafa301f4a0ad0283435c80339b
Sevan Safarian <sevangmb@gmail.com>
2025-08-03 12:39:10 +0000

    tjs pas. met l utilisateur sevans@hotmail.fr admin

--- src/data/mock-data.ts
+++ src/data/mock-data.ts
@@ -36,13 +36,13 @@ const users: User[] = [
   },
   {
     id: 'user2',
-    profile: { displayName: 'CyberLearner', email: 'cyber@example.com', totalPoints: 980, level: 12 },
+    profile: { displayName: 'CyberLearner', email: 'cyber@example.com', totalPoints: 980, level: 12, isAdmin: false },
     competences: {},
     preferences: { learningStyle: 'Kinaesthetic', favoriteTopics: ['Backend'], adaptiveMode: 'Default', language: 'en' }
   },
   {
     id: 'user3',
-    profile: { displayName: 'DataDiva', email: 'data@example.com', totalPoints: 1500, level: 18 },
+    profile: { displayName: 'DataDiva', email: 'data@example.com', totalPoints: 1500, level: 18, isAdmin: false },
     competences: {},
     preferences: { learningStyle: 'Auditory', favoriteTopics: ['Databases', 'AI'], adaptiveMode: 'Objective', language: 'es' }
   }
